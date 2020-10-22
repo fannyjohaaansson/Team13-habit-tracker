9506ee6f16d2cae324f3aed987505e05413dd1c7
Fanny Johansson <fannyjohansson@Fannys-MBP.dhcp.sthlm.hyperisland.local>
2020-10-22 14:48:19 +0200

    Initial Commit

---
+++ effects.js
@@ -0,0 +1,6 @@
+
+let createBtn = document.getElementById("mainHabitButton");
+let popHabit = document.getElementsByClassName("popUp");
+let popTime = document.getElementsByClassName("choose-habit"); 
+let showUp = document.getElementsByClassName("appear");
+
