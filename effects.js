54e1c4132a32916fab846c38832c3740f8fc5815
Fanny Johansson <fannyjohansson@fannys-mbp.lan>
2020-10-27 14:05:50 +0100

    Code merged from extern changes
    The extern table is merged into the main project.

--- effects.js
+++ effects.js
@@ -2,47 +2,58 @@ const addButton = document.querySelector('.addbutton');
 const input = document.querySelector('.input')
 const container = document.querySelector('.container')
 
-class item {
-    constructor(itemName) {
-        this.createDiv(itemName)
+function addRow() {
+    var tableBody = document.querySelector("#table tbody");
+    var tr = document.createElement('tr');
+
+
+    // 1. Add habit table date(td)
+    var td = document.createElement('td');
+    var input = document.createElement('input');
+    var inputVal = document.getElementById("myInput").value;
+    input.value = inputVal;
+    input.disabled = true;
+    input.type = 'text';
+    td.appendChild(input)
+    tr.appendChild(td)
+
+
+
+
+    // Displaying the value
+    // alert(inputVal);
+
+    // 2. Add Start/End
+    var td = document.createElement('td');
+    var inputDate = document.createElement('input');
+    inputDate.type = 'date';
+    td.appendChild(inputDate)
+    tr.appendChild(td)
+    // 3. Add 31 checkboxes
+    for (var i = 0; i < 31; i++) {
+        var td = document.createElement('td');
+        var input = document.createElement('input')
+        input.type = 'checkbox';
+
+        td.appendChild(input)
+        tr.appendChild(td)
     }
-
-    createDiv(itemName) {
-        let input = document.createElement('input');
-        input.value = itemName;
-        input.disabled = true;
-        input.classList.add('item-input');
-        input.type = "text";
-
-        let itemBox = document.createElement('div');
-        itemBox.classList.add('item');
-
-        // let removeButton = document.createElement('button');
-        // removeButton.innerHTML = "REMOVE";
-        // removeButton.classList.add('removeButton');
-
-        container.appendChild(itemBox);
-
-        itemBox.appendChild(input);
-        itemBox.appendChild(removeButton);
-
-        // removeButton.addEventListener('click', () => this.remove(itemBox));
-    }
-
-    remove(item) {
-        container.removeChild(item);
-    }
-}
-function check() {
-    if (input.value != "") {
-        new item(input.value);
-        input.value = "";
-    }
+    tableBody.appendChild(tr);
 
     modal.style.display = "none";
+
+
+
 }
 
-addButton.addEventListener('click', check);
+// function check() {
+//     if (input.value != "") {
+//         new item(input.value);
+//         input.value = "";
+//     }
+// }
+
+addButton.addEventListener('click', addRow);
 window.addEventListener('keydown', (e) => {
     if (e.which == 13) {
         check();
@@ -58,6 +69,7 @@ var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
     modal.style.display = "block";
+
 }
 
 span.onclick = function () {
